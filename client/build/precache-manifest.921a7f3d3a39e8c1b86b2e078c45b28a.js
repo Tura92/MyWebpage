db7167d6b6d901e8f86f1319c554f15e7c9c0e21
Daniel Tura <daniel.tura@live.de>
2019-06-27 22:13:32 +0200

    Backend should work

---
+++ client/build/precache-manifest.921a7f3d3a39e8c1b86b2e078c45b28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89eca627eef58aef4905679116ef4452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba768849a86cee04a899",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "415f8a4c39ecaea70664",
+    "url": "/static/js/2.cf0beb7a.chunk.js"
+  },
+  {
+    "revision": "ba768849a86cee04a899",
+    "url": "/static/js/main.d4ef723e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
